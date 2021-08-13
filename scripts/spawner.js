70189a6629ddb9e2d96041a200def3a80d20c3fc
theripper93 <tsplab@gmail.com>
2021-08-13 22:19:02 +0200

    a

--- scripts/spawner.js
+++ scripts/spawner.js
@@ -4,6 +4,7 @@ class CreatureSpawner {
       case "circle":
         for (let creature of encounterData.creatures) {
           for (let i = 0; i < creature.number; i++) {
+            await CreatureSpawner.wait(100);
             const tD = await creature.actor.getTokenData();
             const position = Propagator.getFreePosition(
               tD,
@@ -23,11 +24,21 @@ class CreatureSpawner {
     }
   }
   //generate a random point in a circle given a center point and a radius
-  static randomInCircle(center, radius) {
-    let angle = Math.random() * Math.PI * 2;
-    radius = Math.random() * radius;
-    let x = Math.cos(angle) * radius + center.x;
-    let y = Math.sin(angle) * radius + center.y;
+  static randomInCircle(center, oradius, collision = true) {
+    
+    let x,y,radius;
+    do{let angle = Math.random() * Math.PI * 2;
+    radius = Math.random() * oradius;
+    x = Math.cos(angle) * radius + center.x;
+    y = Math.sin(angle) * radius + center.y;}while(!collision || canvas.walls.checkCollision(new Ray(center,{x:x,y:y})))
     return { x: x, y: y };
   }
+  //wait for a specific amount of time before returning a promise
+  static wait(time) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve();
+      }, time);
+    });
+}
 }
