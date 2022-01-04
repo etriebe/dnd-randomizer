4c126f4a829d26626a58a955ecd5c3def03a16a1
Eric Triebe <eric.triebe@gmail.com>
2022-01-03 22:11:28 -0800

    Make combat summary conditional

--- scripts/dialog.js
+++ scripts/dialog.js
@@ -37,6 +37,15 @@ class SFDialog extends FormApplication {
 		let $ul = html.find('.form-encounters ul').first();
 
 		for (const encounter of encounterData) {
+			let combatSummaryHTML = ``;
+			if (encounter.combatsummary.totalattacks > 0)
+			{
+				combatSummaryHTML = `<div class="combat-summary">
+					<span class="combat-numberofattacks">${encounter.combatsummary.totalattacks} attacks</span>
+					<span class="combat-totaldamage"> | ${encounter.combatsummary.totaldamage} dmg</span>
+					<span class="combat-averageattackbonus"> | ${encounter.combatsummary.averageattackbonus.toFixed(0)} average attack bonus</span>
+				</div>`;
+			}
 			$ul.append(`<li class="is-favorited-${encounter.data?.id ?? false ? 'true' : 'false' }" data-id="${encounter.id}">
 				<div class="favorite-encounter ${encounter.data?.id ?? false ? 'favorited': ''}"><i class="far fa-star"></i></div>
 				<div class="encounter-details">
@@ -56,11 +65,7 @@ class SFDialog extends FormApplication {
 						<span class="encounter-difficulty ${encounter.data.difficulty}">${encounter.data.difficulty}</span>
 						<span class="encounter-xp">${encounter.data.xp}</span>
 					</div>
-					<div class="combat-summary">
-						<span class="combat-numberofattacks">${encounter.combatsummary.totalattacks} attacks</span>
-						<span class="combat-totaldamage"> | ${encounter.combatsummary.totaldamage} dmg</span>
-						<span class="combat-averageattackbonus"> | ${encounter.combatsummary.averageattackbonus.toFixed(0)} average attack bonus</span>
-					</div>
+					${combatSummaryHTML}
 				</div>
 				<div class="create-encounter">
 					<i class="fas ${game.settings.get(SFCONSTS.MODULE_NAME, 'secretEncounterIcon') ? 'fa-pastafarianism' : 'fa-angle-double-right' }" data-trigger="spawn" title="Spawn Encounter"></i>
