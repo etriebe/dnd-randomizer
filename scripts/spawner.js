b6c3114f2033227a1537a3dc6b8f83da2a3a5d56
Eric Triebe <eric.triebe@gmail.com>
2022-04-26 21:38:33 -0700

    Update to have support for Item Piles and remove support for LootSheetNPC

--- scripts/spawner.js
+++ scripts/spawner.js
@@ -21,8 +21,45 @@ class CreatureSpawner {
             await canvas.scene.createEmbeddedDocuments("Token", [tokenData]);
           }
         }
+
+        let lootActor =  await game.actors.get(encounterData.lootActorId)
+        const tD = await lootActor.getTokenData();
+        const position = Propagator.getFreePosition(
+          tD,
+          CreatureSpawner.randomInCircle(
+            {x:template.data.x,y:template.data.y},
+            (template.data.distance * canvas.dimensions.size) /
+            canvas.dimensions.distance
+          )
+        );
+        const tokenData = await lootActor.getTokenData({
+          x: position.x,
+          y: position.y,
+        });
+        let lootToken = await canvas.scene.createEmbeddedDocuments("Token", [tokenData]);
+        await CreatureSpawner.createItemPiles(lootToken[0]);
     }
   }
+
+  static async createItemPiles(lootToken) {
+    let isItemPilesInstalled = game.modules.get("item-piles");
+    if (!isItemPilesInstalled)
+    {
+      return;
+    }
+
+    let itemPilesIsActive = isItemPilesInstalled.active;
+
+    if (!itemPilesIsActive)
+    {
+      return;
+    }
+
+    // let currentScene = game.scenes.find(s => s.id === canvas.id);
+    // let lootTokenOnScene = currentScene.tokens.find(t => t.id === lootToken.id);
+    await ItemPiles.API.turnTokensIntoItemPiles([lootToken]);
+  }
+
   //generate a random point in a circle given a center point and a radius
   static randomInCircle(center, oradius, collision = true) {
     let attemptNumber = 0;
